c9dad2cc6fab85aa349897e9ef05f300ec626a50
LucasJesus17 <55859221+LucasJesus17@users.noreply.github.com>
2022-01-14 12:14:46 -0300

    Iniciando no React e Material ui

---
+++ Modulo3/BlogPessoal/blog-pessoal/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+import Home from './paginas/home/Home';
+
+
+function App() {
+  return (
+    <>
+    <Home/>
+
+    </>
+
+
+  );
+}
+
+export default App;
